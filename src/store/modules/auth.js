d2900d3957cb22c03db16a86ef5455c375fd1266
Nathan Herrera <nathanherrera@gmail.com>
2017-07-27 18:52:51 -0700

    Adds mutations to update user and auth objects in the store

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -18,6 +18,24 @@ else {
   }
 }
 
+const mutations = {
+  updateUser (state, user) {
+    state.user = user
+  },
+  updateAuth (state, auth) {
+    let newAuthHeaders = {
+      accessToken: auth.headers.accessToken,
+      tokenType: auth.headers.tokenType,
+      client: auth.headers.client,
+      expiry: auth.headers.expiry,
+      uid: auth.headers.uid
+    }
+    if (!state.auth.isLoggedIn) { state.auth.isLoggedIn = true }
+    state.auth.headers = newAuthHeaders
+  }
+}
+
 export default {
-  state: initialState
+  state: initialState,
+  mutations
 }
