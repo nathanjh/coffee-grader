f0d76701440a84cf0a37527b5afe4a1f9eb6cfb8
Nathan Herrera <nathanherrera@gmail.com>
2017-07-30 23:53:07 -0700

    Clean up unneccessary comments

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -17,41 +17,16 @@
     <router-view class="layout-view"></router-view>
 
     <session-modal :modalOpen="modalOpen"></session-modal>
-    <!--
-      Replace following "div" with
-      "<router-view class="layout-view">" component
-      if using subRoutes
-    -->
-    <!-- <q-modal ref="signinModal" :content-css="{minWidth: '70vw', minHeight: '60vh'}">
-    <q-layout>
-      <div slot="header" class="toolbar">
-        <q-toolbar-title class="text-center">Sign In</q-toolbar-title>
-        <button @click="$refs.signinModal.close()">
-          <i>close</i>
-        </button>
-      </div>
-
-      <div class="layout-view">
-        <div class="layout-padding">
-          <sign-in></sign-in>
-        </div>
-      </div>
-    </q-layout>
-  </q-modal> -->
-
-
   </q-layout>
 </template>
 
 <script>
-  // import signIn from './session/SignIn'
   import sessionModal from './session/SessionModal'
   import { mapGetters } from 'vuex'
   import { Toast } from 'quasar'
 
   export default {
     components: {
-      // signIn
       sessionModal
     },
     computed: {
@@ -80,16 +55,5 @@
   }
 </script>
 
-<style lang="stylus">
-.logo-container
-  width 192px
-  height 268px
-  perspective 800px
-  position absolute
-  top 50%
-  left 50%
-  transform translateX(-50%) translateY(-50%)
-.logo
-  position absolute
-  transform-style preserve-3d
+<style lang="css">
 </style>
