e658b2f526706d96f27e9086aa60c4474b876ce7
Nathan Herrera <nathanherrera@gmail.com>
2017-09-01 11:21:16 -0700

    Updates unit test index to use vuelidate/quasar, mock injected global components

--- test/unit/specs/SignUp.spec.js
+++ test/unit/specs/SignUp.spec.js
@@ -1,12 +1,27 @@
-// import Vue from 'vue'
+import Vue from 'vue'
 // import { mount } from 'vue-test-utils'
 import SignUp from '../../../src/components/session/SignUp'
+import { eventBus } from '../../../src/eventBus'
+
+const Constructor = Vue.extend(SignUp)
 
 describe('SignUp.vue', () => {
-  it('has a created hook', () => {
-    // const Constructor = Vue.extend(SignUp)
-    // const vm = new Constructor().$mount()
+  describe('lifecycle hooks: created', () => {
+    it('has a created hook', () => {
+      expect(SignUp.created).to.be.a('function')
+    })
+    it("listens for a 'clearForm' event", () => {
+      const vm = new Constructor({
+        data: {
+          form: {
+            name: 'whatever'
+          }
+        }
+      }).$mount()
 
-    expect(SignUp.created).to.be.a('function')
+      expect(vm.form.name).to.eq('whatever')
+      eventBus.$emit('clearForm')
+      expect(vm.form.name).to.eq('')
+    })
   })
 })
